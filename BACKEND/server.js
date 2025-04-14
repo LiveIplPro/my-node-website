2074f6cce58d4f4876c49f25535e1a01362f992d
LiveIplPro <kartiksolanki2025@gmail.com>
2025-04-14 11:53:49 +0530

    Update server.js

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -7,10 +7,7 @@ const rateLimit = require("express-rate-limit");
 
 const app = express();
 const PORT = 3001;
-
-const express = require('express');
 const path = require('path');
-const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from PUBLIC folder
